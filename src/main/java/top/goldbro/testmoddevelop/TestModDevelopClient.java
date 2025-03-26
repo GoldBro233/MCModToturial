c4eb0ace9becc19a395571e0d7d9d5111bc98d22
GoldBro233 <bowensun_06@outlook.com>
2025-03-26 19:54:39 +0800

    Init Commit

---
+++ src/main/java/top/goldbro/testmoddevelop/TestModDevelopClient.java
@@ -0,0 +1,8 @@
+package top.goldbro.testmoddevelop;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TestModDevelopClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {}
+}
